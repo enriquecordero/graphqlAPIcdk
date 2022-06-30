9af54eebb3ef04305ed1457ab2bbc72a60fa53c8
Enrique Cordero <ecs5000@gmail.com>
2022-06-30 17:06:30 -0400

    init

--- lib/api-stack.ts
+++ lib/api-stack.ts
@@ -1,16 +1,49 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import {  Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import {Code, Function, Runtime} from 'aws-cdk-lib/aws-lambda'
+import {Cors, LambdaRestApi} from 'aws-cdk-lib/aws-apigateway'
+import {join} from 'path'
+
+
 
 export class ApiStack extends Stack {
+  
+  private lambda: Function
+  private api: LambdaRestApi
+  
   constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    super(scope, id, props);  
 
-    // The code that defines your stack goes here
+   this.lambda = this.createFrontEndAPILambda();
+   this.createLambdaRestApi(this.lambda)
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'ApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+ 
+  }
+
+  createLambdaRestApi(fn:Function):LambdaRestApi{
+
+    this.api =  new LambdaRestApi(this,'Lambda Api Graphql',{
+      handler:fn,
+   //   deployOptions:{stageName:'dev'},
+      // defaultCorsPreflightOptions:{
+      //   allowOrigins: Cors.ALL_ORIGINS
+      // },           
+    });
+
+ 
+    return this.api
+  }
+
+
+  createFrontEndAPILambda():Function{
+    this.lambda = new Function(this,'LambdaFunction',{
+      functionName: 'LambdaFunction',
+      runtime:Runtime.NODEJS_14_X,
+      handler: 'index.graphqlHandler',
+      code: Code.fromAsset(join(__dirname,'../lambda'))
+    }) 
+    return this.lambda;
   }
 }
+
+
